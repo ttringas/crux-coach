8bb13b1128e2117f8a318673a350441dfb53b369
Tyler Tringas <tyler@tylertringas.com>
2026-03-28 21:58:13 -0400

    Redirect new users to onboarding, guard all routes until onboarded

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,33 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, unless: :public_page?
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :ensure_onboarded!, unless: :skip_onboarding_check?
 
   helper_method :current_climber_profile
 
   protected
 
-  def after_sign_in_path_for(_resource)
-    dashboard_path
+  def after_sign_in_path_for(resource)
+    if resource.climber_profile&.onboarding_completed?
+      dashboard_path
+    else
+      onboarding_path(1)
+    end
   end
 
   def after_sign_up_path_for(_resource)
     onboarding_path(1)
+  end
+
+  def ensure_onboarded!
+    return unless user_signed_in?
+    return if current_user.climber_profile&.onboarding_completed?
+
+    redirect_to onboarding_path(1) unless request.path.start_with?("/onboarding")
+  end
+
+  def skip_onboarding_check?
+    devise_controller? || public_page? || (controller_name == "onboarding")
   end
 
   def current_climber_profile
